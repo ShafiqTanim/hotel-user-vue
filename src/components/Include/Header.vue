3ec2632f684439fe6c26abfefca92e04f0ce6a7e
ShafiqTanim <shafiqullah715@gmail.com>
2024-11-27 01:24:29 +0600

    theme partition compleated

--- src/components/Include/Header.vue
+++ src/components/Include/Header.vue
@@ -1,27 +1,27 @@
 <template>
   <header id="header">
-    <a href="index.html" class="logo"><img src="assets/img/logo.jpg" alt="logo"></a>
+    <a href="http://localhost:8081/" class="logo"><img src="assets/img/logo.jpg" alt="logo"></a>
     <div class="phone"><i class="fa fa-phone"></i>+1 234 567 8900</div>
     <div class="mobile-menu-btn"><i class="fa fa-bars"></i></div>
     <nav class="main-menu top-menu">
       <ul>
         <li class="active"><a href="http://localhost:8081/">Home</a></li>
-        <li><a href="about.html">About Us</a></li>
+        <li><a href="http://localhost:8081/about">About Us</a></li>
         <li><a href="http://localhost:8081/room">Rooms</a></li>
-        <li><a href="amenities.html">Amenities</a></li>
+        <li><a href="http://localhost:8081/amenities">Amenities</a></li>
         <li><a href="http://localhost:8081/booking">Booking</a></li>
-        <li><a href="login.html">Login</a></li>
-        <li><a href="contact.html">Contact Us</a></li>
+        <li><a href="http://localhost:8081/login">Login</a></li>
+        <li><a href="http://localhost:8081/contact">Contact Us</a></li>
       </ul>
     </nav>
   </header>
 </template>
 
 <script>
-  export default{
-      name: 'Header',
-      props: {
-        msg: String
-      }
+  export default {
+    name: 'Header',
+    props: {
+      msg: String
+    }
   }
 </script>
--- src/router.js
+++ src/router.js
@@ -8,17 +8,41 @@ const routes = [
         component: () => import("./components/Home")
     },
     {
+        path: "/about",
+        alias: "/about",
+        name: "about",
+        component: () => import("./components/About")
+    },
+    {
         path: "/room",
         alias: "/room",
         name: "room",
         component: () => import("./components/Room")
     },
     {
+        path: "/amenities",
+        alias: "/amenities",
+        name: "amenities",
+        component: () => import("./components/Amenities")
+    },
+    {
         path: "/booking",
         alias: "/booking",
         name: "booking",
         component: () => import("./components/Booking")
-    }
+    },
+    {
+        path: "/login",
+        alias: "/login",
+        name: "login",
+        component: () => import("./components/Login")
+    },
+    {
+        path: "/contact",
+        alias: "/contact",
+        name: "contact",
+        component: () => import("./components/Contact")
+    },
 ];
 
 const router = createRouter({
